17b0f9e244e205382b67b303a898bf0fe7057593
akoobeeEy <119799055+akoobeeEy@users.noreply.github.com>
2024-11-06 23:01:52 +0500

    redux bug fixed

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit";
 import shoppersReducer from "./shoppersSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key: "shoppers",
+  storage,
+};
 
+const persistedReducer = persistReducer(persistConfig, shoppersReducer);
 export const store = configureStore({
   reducer: {
-    shoppers: shoppersReducer,
+    shoppers: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
