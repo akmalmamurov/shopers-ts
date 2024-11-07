ef6339b2dddc2750a17f9259414a721b5533367d
akoobeeEy <119799055+akoobeeEy@users.noreply.github.com>
2024-11-07 15:13:20 +0500

    storage restart

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -9,8 +9,34 @@ import {
   PERSIST,
   PURGE,
   REGISTER,
+  WebStorage,
 } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import createWebStorage from "redux-persist/es/storage/createWebStorage";
+
+export function createPersistStorage(): WebStorage {
+  const isServer = typeof window === "undefined";
+  // We will create dummy server
+  if (isServer) {
+    return {
+      getItem() {
+        return Promise.resolve(null);
+      },
+      setItem() {
+        return Promise.resolve();
+      },
+      removeItem() {
+        return Promise.resolve();
+      },
+    };
+  }
+  return createWebStorage("local");
+}
+
+const storage =
+  typeof window !== "undefined"
+    ? createWebStorage("local")
+    : createPersistStorage();
+
 const persistConfig = {
   key: "shoppers",
   storage,
